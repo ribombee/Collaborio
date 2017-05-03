ab4030180904adeaae6d3a16f9e491f3925ce68d
ke1337 <hrafnkelli14@ru.is>
2017-05-03 15:04:38 +0000

    theme picker for monaco

---
+++ VLN2-H27/VLN2-H27/Scripts/MonacoScripts.js
@@ -0,0 +1,27 @@
+﻿
+require.config({ paths: { 'vs': '../../monaco/min/vs' } });
+
+function changeTheme(theme, editor) {
+    alert("dingus");
+    var newTheme = (theme === 1 ? 'vs-dark' : (theme === 0 ? 'vs' : 'hc-black'));
+    editor.updateOptions({ 'theme': newTheme }); 
+}
+
+require(['vs/editor/editor.main'], function () {
+    var editor = monaco.editor.create(document.getElementById('container'), {
+        value: [
+            'function x() {',
+            '\tconsole.log("Hello world!");',
+            '}'
+        ].join('\n'),
+        language: 'javascript'
+    });
+
+    $(".theme-picker").change(function () {
+        changeTheme(this.selectedIndex, editor);
+    });
+});
+
+
+
+
