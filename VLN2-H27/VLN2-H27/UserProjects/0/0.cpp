86a0ea3434dafc54ae7e3e4416aa339bf8cf4ba0
ke1337 <hrafnkelli14@ru.is>
2017-05-08 16:39:06 +0000

    fixed editor sync hopefully

--- VLN2-H27/VLN2-H27/UserProjects/0/0.cpp
+++ VLN2-H27/VLN2-H27/UserProjects/0/0.cpp
@@ -6,6 +6,7 @@ cccccccccc testbulk sdfsdfsdfsdfdsfretgott test
 
 dsfdsfdsfsdf
 
+<<<<<<< HEAD
 adsadsadasdsa
 
 asdasdsa
@@ -17,11 +18,37 @@ sdfsd
 
 fdgdfgsdfgdfg
 dsfdsfds
+=======
+
+
+>>>>>>> 9c029f8bda581dfb231f86c661b0d32ddb520200
 fak
+Car car = new Car;
+
+valliisalovlyguy@sexyluv.com
+:O
+var 
+asososidufoaospdifusosao hæ óli hahahahahahahahahhahahahhah
+XDXDXDDXDXDXDXDXDXDXDXDXDXDDX
+
+var x = "thranduil";
+var y = "legolas";
+hello my name is paul newman
+var z = "Smaug";
+
+for(int i = 0; i < size; i++)
+hello world i'm fagor the destroyer
+{
+    
+}
+
+
+cccccccccccccccccccccccc
 
 sdfsd
 
 adsadasdasdsasadas
 
 
-cccccccccccccccccccccccc
+jfsldjgldf-jigldfklljkasdjljhallolkjlfsdlajflsajf
+for(int i = 0; i < 123; i++iei eakgdae erjafsdglireaadhf erausdafssdfewfasdsefrasd
